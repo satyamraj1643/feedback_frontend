fbe57b0e503ce360c3e910a9c2528ac47e78de24
satyamraj1643 <your_email@example.com>
2024-02-07 22:33:47 +0530

    Card

--- src/assets/Card.js
+++ src/assets/Card.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 const Card = ({ feedback }) => {
   return (
-    <div className="bg-white border rounded-lg shadow-lg p-6">
+    <div className="bg-white border rounded-lg shadow-lg p-6 min-w-0 max-w-md">
       <p><strong>Feedback:</strong> {feedback.content}</p>
       <p><strong>Feedback ID:</strong> {feedback.feedbackId}</p>
       <p><strong>Type:</strong> {feedback.feedbackType}</p>
       <p> By - {feedback.firstName} {feedback.lastName} </p>
       <p><strong>Created At:</strong> {new Date(feedback.createdAt).toLocaleString()}</p>
-    
     </div>
   );
 };
